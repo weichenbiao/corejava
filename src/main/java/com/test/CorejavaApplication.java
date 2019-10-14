be7b1a9ea3c53c72bd252479463720f1c9f84e60
weichenbiao <56472956+weichenbiao@users.noreply.github.com>
2019-10-14 12:44:36 +0800

    Update CorejavaApplication.java

--- src/main/java/com/test/CorejavaApplication.java
+++ src/main/java/com/test/CorejavaApplication.java
@@ -9,6 +9,7 @@ public class CorejavaApplication {
     public static void main(String[] args) {
         SpringApplication.run(CorejavaApplication.class, args);
         System.out.println("11111");
+        System.out.println("22222");
     }
 
 }
