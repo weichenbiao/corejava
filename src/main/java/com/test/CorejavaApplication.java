490780d65bbd7264b4c86f3eee6d0c96595bad3e
wcb <1493140970@qq.com>
2019-10-14 12:40:01 +0800

    v1.1

--- src/main/java/com/test/CorejavaApplication.java
+++ src/main/java/com/test/CorejavaApplication.java
@@ -8,6 +8,7 @@ public class CorejavaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CorejavaApplication.class, args);
+        System.out.println("11111");
     }
 
 }
